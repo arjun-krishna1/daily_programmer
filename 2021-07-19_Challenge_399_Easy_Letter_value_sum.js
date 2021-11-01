ee14499c2f423cdd19d41f155c7a5e3a0f14e423
arjun krishna <arjunkrishna1306@gmail.com>
2021-11-01 15:21:19 -0400

    complete letter value sum challenge

--- 2021-07-19_Challenge_399_Easy_Letter_value_sum.js
+++ 2021-07-19_Challenge_399_Easy_Letter_value_sum.js
@@ -8,8 +8,17 @@ OUT
 sum of values of the letters in the string
 */
 
+// create a buffer so the value of "a" is 1
+let buffer = "a".charCodeAt(0) - 1;
 const letterValueSum = (str) => {
-    console.log("hello world");
+    let res = 0;
+    for (let i = 0; i < str.length; i++) {
+        res += str.charCodeAt(i) - buffer;
+    }
+    return res;
 }
 
-letterValueSum("aaa");
+console.log(letterValueSum("aaa") === 3);
+console.log(letterValueSum("aba") === 4);
+console.log(letterValueSum("aba") === letterValueSum("baa"));
+console.log(letterValueSum("zzzz") === 26*4);
